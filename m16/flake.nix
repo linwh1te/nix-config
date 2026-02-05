c22b794ff3e4ec302226778e64465e2f3fd665c4
linwh1te <linwhite@linwhite.top>
2026-02-05 22:01:31 +0800

    changed sometings

---
+++ m16/flake.nix
@@ -0,0 +1,17 @@
+{
+  description = "A simple NixOS flake";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
+  };
+
+  outputs = { self, nixpkgs, ... }@inputs: {
+    nixosConfigurations.m16 = nixpkgs.lib.nixosSystem {
+      system = "x86_64-linux";
+      specialArgs = { inherit inputs; };
+      modules = [
+        ./configuration.nix
+      ];
+    };
+  };
+}
