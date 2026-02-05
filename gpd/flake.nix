1a7979625faebf9924c6d533cc1fae8fa4254c5c
linwh1te <linwhite@linwhite.top>
2026-02-05 02:06:35 +0000

    new GPD

--- gpd/flake.nix
+++ gpd/flake.nix
@@ -6,7 +6,9 @@
   };
 
   outputs = { self, nixpkgs, ... }@inputs: {
-    nixosConfigurations.my-nixos = nixpkgs.lib.nixosSystem {
+    nixosConfigurations.gpd = nixpkgs.lib.nixosSystem {
+      system = "x86_64-linux";
+      specialArgs = { inherit inputs; };
       modules = [
         ./configuration.nix
       ];
